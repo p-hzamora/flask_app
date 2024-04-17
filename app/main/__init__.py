db0c58a7dc7ea9c243ba3fb4f77917d9f8444304
p-hzamora <p.hzamora@icloud.com>
2024-04-17 14:18:03 +0200

    style: applied Ruff

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 
-bp = Blueprint("main", __name__, template_folder="templates",static_folder="static", static_url_path="/main/static")
+bp = Blueprint(
+    "main",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/main/static",
+)
 
-from app.main import routes
+from app.main import routes  # noqa: E402, F401
